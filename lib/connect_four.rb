2b2c8620990bbbd0785b9bb58fe24833c8f13c65
HereIsSee <biramantvydas@gmail.com>
2024-08-13 00:14:22 +0300

    RuboCop syntax fix

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,10 +1,6 @@
 class ConnectFour
-
   def initialize
-
     @array = Array.new(7) { Array.new(6) }
-
-    
   end
 
   def play
@@ -12,27 +8,23 @@ class ConnectFour
     until win? || board_full?
       puts "Player #{player_number} make your move (choose column between 1 to 7):"
       show_grid
-      
+
       column = ''
       loop do
         column = gets.chomp
-        if !column.to_i.between?(1,7) || column_full?(column.to_i - 1)
-          puts "Wrong input!" 
-        else
-          break
-        end
-      end
-      
-      move('x', column.to_i - 1) if player_number == 1
-      move('o', column.to_i - 1 ) if player_number == 2
+        break unless !column.to_i.between?(1, 7) || column_full?(column.to_i - 1)
 
-      if win?
-        return puts "Player #{player_number} wins!"
+        puts 'Wrong input!'
       end
+
+      move('x', column.to_i - 1) if player_number == 1
+      move('o', column.to_i - 1) if player_number == 2
+
+      return puts "Player #{player_number} wins!" if win?
 
       player_number = player_number == 1 ? 2 : 1
     end
-    puts "Tie!"
+    puts 'Tie!'
   end
 
   def board_full?
@@ -46,18 +38,16 @@ class ConnectFour
     return true if horizontal_win?
     return true if vertical_win?
     return true if diagonal_win?
+
     false
   end
-  
+
   def vertical_win?
     @array.each do |column|
-
       next if column.compact.size < 4
 
       column.each_cons(4) do |segment|
-        if segment.all? { |value| value == segment[0] && !value.nil? }
-          return true
-        end
+        return true if segment.all? { |value| value == segment[0] && !value.nil? }
       end
     end
     false
@@ -65,12 +55,10 @@ class ConnectFour
 
   def horizontal_win?
     transposed_array = @array.transpose
-  
+
     transposed_array.each do |row|
       row.each_cons(4) do |segment|
-        if segment.all? { |value| value == segment[0] && !value.nil? }
-          return true
-        end
+        return true if segment.all? { |value| value == segment[0] && !value.nil? }
       end
     end
     false
@@ -79,31 +67,30 @@ class ConnectFour
   def diagonal_win?
     (0..3).each do |col|
       (0..2).each do |row|
-        if @array[col][row] && 
-           @array[col][row] == @array[col + 1][row + 1] && 
-           @array[col][row] == @array[col + 2][row + 2] && 
+        if @array[col][row] &&
+           @array[col][row] == @array[col + 1][row + 1] &&
+           @array[col][row] == @array[col + 2][row + 2] &&
            @array[col][row] == @array[col + 3][row + 3]
           return true
         end
       end
     end
-  
+
     (0..3).each do |col|
       (3..5).each do |row|
-        if @array[col][row] && 
-           @array[col][row] == @array[col + 1][row - 1] && 
-           @array[col][row] == @array[col + 2][row - 2] && 
+        if @array[col][row] &&
+           @array[col][row] == @array[col + 1][row - 1] &&
+           @array[col][row] == @array[col + 2][row - 2] &&
            @array[col][row] == @array[col + 3][row - 3]
           return true
         end
       end
     end
-  
+
     false
   end
-  
 
-  def move(sign, column)   
+  def move(sign, column)
     row = @array[column].index(nil)
     @array[column][row] = sign
     @array[column]
@@ -111,21 +98,21 @@ class ConnectFour
 
   def column_full?(column)
     return true if @array[column].compact.size >= 6
+
     false
   end
 
   def show_grid
     # Iterate from top row (index 5) to bottom row (index 0)
-    (5).downto(0) do |row_index|
-      row = ""
+    5.downto(0) do |row_index|
+      row = ''
       # Iterate over each column (from 0 to 6) to get the value in the current row
-      (0).upto(6) do |col_index|
-        value = @array[col_index][row_index] || "-"
-        row = row.concat(value, " ")
+      0.upto(6) do |col_index|
+        value = @array[col_index][row_index] || '-'
+        row = row.concat(value, ' ')
       end
       puts row
     end
-    puts ""
+    puts ''
   end
-
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,9 +1,8 @@
 require './lib/connect_four'
 
 describe ConnectFour do
-  
   describe '#move' do
-  subject(:connect_four) { described_class.new }
+    subject(:connect_four) { described_class.new }
 
     it 'makes first move x in column 3' do
       column = connect_four.move('x', 3)
@@ -14,11 +13,9 @@ describe ConnectFour do
     it 'makes second move o in column 3' do
       connect_four.move('x', 3)
       column = connect_four.move('o', 3)
-      
 
       expect(column[1]).to eq('o')
     end
-  
   end
 
   describe '#win?' do
@@ -49,11 +46,11 @@ describe ConnectFour do
 
       connect_four_win.move('o', 1)
       connect_four_win.move('x', 1)
-      
+
       connect_four_win.move('x', 2)
       connect_four_win.move('x', 2)
       connect_four_win.move('x', 2)
-      
+
       connect_four_win.move('o', 3)
       connect_four_win.move('o', 3)
       connect_four_win.move('x', 3)
@@ -69,11 +66,11 @@ describe ConnectFour do
 
       connect_four_win.move('o', 2)
       connect_four_win.move('x', 2)
-      
+
       connect_four_win.move('x', 1)
       connect_four_win.move('x', 1)
       connect_four_win.move('x', 1)
-      
+
       connect_four_win.move('o', 0)
       connect_four_win.move('o', 0)
       connect_four_win.move('x', 0)
@@ -84,6 +81,4 @@ describe ConnectFour do
       expect(connect_four_win).to be_win
     end
   end
-  
-  
 end
